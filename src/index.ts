472976c43350ad70c15efbe86fdbe9fcf93d02b7
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2022-01-12 16:13:00 +0200

    Add abbreviation finder & test cases

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,25 @@
-export const example = (): string => 'test string';
+import { getAbbreviationMapping } from './mappings/abbr-mapping';
 
+export const findFromMapping = (
+  content: string,
+  abbreviations: Map<string, string>,
+) : Map<string, string> => {
+  const result: Map<string, string> = new Map();
+
+  abbreviations.forEach((value, key) => {
+    if (content.includes(key)) {
+      result.set(key, value);
+    }
+  });
+
+  return result;
+};
+
+export const findAbbreviations = (content: string) : Map<string, string> => {
+  const abbrMap = getAbbreviationMapping();
+  return findFromMapping(content, abbrMap);
+};
 export default {
-  example,
+  getAbbreviationMapping,
+  findAbbreviations,
 };
